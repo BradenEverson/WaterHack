f41e1c971b327ac9ff91be58306fcdfde8cb2723
Braden Everson <bradeneverson@gmail.com>
2020-11-14 11:00:21 -0600

    First file upload with custom bootstrap theme implemented

---
+++ WaterHack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WaterHack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
